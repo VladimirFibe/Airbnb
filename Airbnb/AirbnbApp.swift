6ee8f49bbd9a47dd3a24976ef7bb8a4bd534d7ba
Vladimir Fibe <motiw@icloud.com>
2023-10-20 09:36:04 +0600

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Vladimir Fibe on 20.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
