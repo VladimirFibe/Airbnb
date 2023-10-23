4aa253d4a4c6e9a1eac387c8f2d13a9068e0f957
Vladimir Fibe <motiw@icloud.com>
2023-10-23 15:33:49 +0600

    Wishlists

--- Airbnb/ContentView.swift
+++ Airbnb/ContentView.swift
@@ -3,9 +3,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
             TabView {
+                ExploreView()
                 WishListView()
                 ProfileView()
-                ExploreView()
             }
     }
 }
--- Airbnb/WishListView.swift
+++ Airbnb/WishListView.swift
@@ -2,7 +2,38 @@ import SwiftUI
 
 struct WishListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 32.0) {
+                VStack(alignment: .leading, spacing: 8.0) {
+                    Text("Login to view your wishlists")
+                        .font(.headline)
+                    Text("You can create, view or edit wishlists once you've logged in")
+                        .font(.footnote)
+                }
+                Button(action: {}) {
+                    Text("Log in")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(height: 48)
+                        .frame(maxWidth: .infinity)
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.pink)
+
+                HStack {
+                    Text("Don't have an account?")
+
+                    Text("Sign up")
+                        .fontWeight(.semibold)
+                        .underline()
+                }
+                .font(.caption)
+
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Wishlist")
+        }
     }
 }
 
